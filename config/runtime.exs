a3a05c971d3922d5e8641f59a999aae006fe701e
Daniel <danieldev0353@outlook.com>
2021-05-21 11:01:04 +0600

    [task-2] Add deploy workflow with heroku

--- config/runtime.exs
+++ config/runtime.exs
@@ -13,7 +13,7 @@ if config_env() == :prod do
       """
 
   config :elixir_search_extractor, ElixirSearchExtractor.Repo,
-    # ssl: true,
+    ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
