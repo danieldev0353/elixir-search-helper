26d648366fedf2fecd9803fa6c9a13e46b117336
Daniel <danieldev0353@outlook.com>
2021-05-27 15:27:21 +0600

    [task-4] Create user registration.

---
+++ test/support/fixtures/accounts_fixtures.ex
@@ -0,0 +1,34 @@
+defmodule ElixirSearchExtractor.AccountsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `ElixirSearchExtractor.Accounts` context.
+  """
+
+  def valid_user_name, do: "Test User"
+  def unique_user_email, do: "user#{System.unique_integer()}@example.com"
+  def valid_user_password, do: "hello world!"
+
+  def valid_user_attributes(attrs \\ %{}) do
+    Enum.into(attrs, %{
+      email: unique_user_email(),
+      name: valid_user_name(),
+      password: valid_user_password(),
+      password_confirmation: valid_user_password()
+    })
+  end
+
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> valid_user_attributes()
+      |> ElixirSearchExtractor.Accounts.register_user()
+
+    user
+  end
+
+  def extract_user_token(fun) do
+    {:ok, captured} = fun.(&"[TOKEN]#{&1}[TOKEN]")
+    [_, token, _] = String.split(captured.body, "[TOKEN]")
+    token
+  end
+end
