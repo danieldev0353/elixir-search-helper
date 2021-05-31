34cdda01fbcb9ed10cd23acc4547ed1fc4c600b1
Daniel <danieldev0353@outlook.com>
2021-06-01 00:28:56 +0600

    [task-10] Remove unnecessary route

--- lib/elixir_search_extractor_web/router.ex
+++ lib/elixir_search_extractor_web/router.ex
@@ -50,7 +50,6 @@ defmodule ElixirSearchExtractorWeb.Router do
     get "/", PageController, :index
     get "/users/settings", UserSettingsController, :edit
     put "/users/settings", UserSettingsController, :update
-    get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
   end
 
   scope "/", ElixirSearchExtractorWeb do
