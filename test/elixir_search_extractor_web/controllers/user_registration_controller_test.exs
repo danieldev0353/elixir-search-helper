932f2644b9f06fc357f37387f240ae54f3164bc7
Daniel <danieldev0353@outlook.com>
2021-06-02 02:43:55 +0600

    [task-5] Create frontend for registration.

--- test/elixir_search_extractor_web/controllers/user_registration_controller_test.exs
+++ test/elixir_search_extractor_web/controllers/user_registration_controller_test.exs
@@ -7,7 +7,7 @@ defmodule ElixirSearchExtractorWeb.UserRegistrationControllerTest do
     test "renders registration page", %{conn: conn} do
       conn = get(conn, Routes.user_registration_path(conn, :new))
       response = html_response(conn, 200)
-      assert response =~ "<h1>Open an account!</h1>"
+      assert response =~ "<h4>Open an account!</h4>"
     end
 
     test "redirects if already logged in", %{conn: conn} do
@@ -47,7 +47,7 @@ defmodule ElixirSearchExtractorWeb.UserRegistrationControllerTest do
         })
 
       response = html_response(conn, 200)
-      assert response =~ "<h1>Open an account!</h1>"
+      assert response =~ "<h4>Open an account!</h4>"
       assert response =~ "should be at least 5 character"
       assert response =~ "must have the @ sign and no spaces"
       assert response =~ "should be at least 6 character"
