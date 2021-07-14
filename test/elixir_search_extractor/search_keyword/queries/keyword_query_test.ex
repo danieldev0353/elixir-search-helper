5350323e6a0e5277b8b20c59345fc67809fe2aa7
Daniel <danieldev0353@outlook.com>
2021-07-14 09:48:33 +0600

    [task-18] remove unnecessary it in test name

--- test/elixir_search_extractor/search_keyword/queries/keyword_query_test.ex
+++ test/elixir_search_extractor/search_keyword/queries/keyword_query_test.ex
@@ -4,7 +4,7 @@ defmodule ElixirSearchExtractor.SearchKeyword.Queries.KeywordQueryTest do
   alias ElixirSearchExtractor.SearchKeyword.Queries.KeywordQuery
 
   describe "filter_keywords/2" do
-    test "it returns unmodified query if the second argument is nil" do
+    test "returns unmodified query if the second argument is nil" do
       query = "SELECT * FROM keyword_files"
 
       returned_query = KeywordQuery.filter_keywords(query, nil)
