8df42f4b9ab80b782d02178282c1fdf8df89bcdd
Daniel <danieldev0353@outlook.com>
2021-07-14 18:46:42 +0600

    [task-21] remove unnecessary variable

--- lib/elixir_search_extractor_web/helpers/ecto_error_beautifier.ex
+++ lib/elixir_search_extractor_web/helpers/ecto_error_beautifier.ex
@@ -1,12 +1,9 @@
 defmodule ElixirSearchExtractorWeb.Helpers.EctoErrorBeautifier do
   def beautify_ecto_error(changeset) do
-    out =
-      Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
-        Enum.reduce(opts, msg, fn {key, value}, acc ->
-          String.replace(acc, "%{#{key}}", to_string(value))
-        end)
+    Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
       end)
-
-    out
+    end)
   end
 end
