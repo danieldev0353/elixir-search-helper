3c88147f2ecd40246ac00bd5321acb25419f4c6b
Daniel <danieldev0353@outlook.com>
2021-07-15 15:55:40 +0600

    [task-21] Fix failing test

--- test/elixir_search_extractor_web/plugs/set_current_api_user_plug_test.exs
+++ test/elixir_search_extractor_web/plugs/set_current_api_user_plug_test.exs
@@ -14,11 +14,10 @@ defmodule ElixirSearchExtractorWeb.SetCurrentApiUserPlugTest do
   describe "call/2" do
     test "assigns logged in api user to conn.assign as current_user", %{conn: conn} do
       user = user_fixture()
-      access_token = access_token_fixture(user)
 
       conn =
         conn
-        |> put_req_header("authorization", "Bearer " <> access_token.token)
+        |> set_authentication_header(user)
         |> post(Routes.api_v1_keyword_file_path(conn, :create), %{})
 
       assert conn.assigns.current_user.id == user.id
