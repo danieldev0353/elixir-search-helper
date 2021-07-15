5218cfb34cb6c75587c86f1f8b1dec773e05fc27
Daniel <danieldev0353@outlook.com>
2021-07-15 17:00:39 +0600

    [task-22-23] remove unneeded query

--- lib/elixir_search_extractor/search_keyword/queries/keyword_query.ex
+++ lib/elixir_search_extractor/search_keyword/queries/keyword_query.ex
@@ -9,12 +9,6 @@ defmodule ElixirSearchExtractor.SearchKeyword.Queries.KeywordQuery do
     |> order_by([k, _], desc: k.inserted_at)
   end
 
-  def user_keyword(user, keyword_id) do
-    Keyword
-    |> join(:inner, [k], kf in assoc(k, :keyword_file))
-    |> where([k, kf], k.id == ^keyword_id and kf.user_id == ^user.id)
-  end
-
   def filter_keywords(query, nil), do: query
 
   def filter_keywords(query, search_string) do
